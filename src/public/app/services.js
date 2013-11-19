a006622c171aa8174fee34c64f88668b97af937c
Aaron Powell <me@aaron-powell.com>
2013-11-18 16:11:37 -0800

    Implementing log walker

--- src/public/app/services.js
+++ src/public/app/services.js
@@ -20,8 +20,25 @@
                         if (err) {
                             return d.reject(err);
                         }
-                        
-                        return d.resolve(repo);
+
+                        var logs = [];
+
+                        repo.logWalk('HEAD', function (err, stream) {
+                            stream.read(function walker(err, log) {
+                                if (err) {
+                                    return d.reject(err);
+                                }
+                                if (log) {
+                                    logs.push(log);
+                                    return stream.read(walker);
+                                }
+
+                                d.resolve({
+                                    name: repo.name,
+                                    logs: logs
+                                });
+                            });
+                        });
                     });
 
                     return d.promise;
